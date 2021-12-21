60deb6c782b2a4a47b15ba3ce13469d4ab93c9ef
teslo2004 <teslo2004@gmail.com>
2021-12-21 11:51:06 +0300

    ADD: add item in list

--- src/App.js
+++ src/App.js
@@ -3,29 +3,47 @@ import Content from './components/Content';
 import Footer from './components/Footer';
 import Headers from './components/Headers';
 import { useState } from 'react';
+import AddItem from './components/AddItem';
 
 function App() {
-  const [items, setItems] = useState([
-    { id: 1, checked: true, item: 'Item 1' },
-    { id: 2, checked: false, item: 'Item 2' },
-    { id: 3, checked: false, item: 'Item 3' },
-  ]);
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')));
+
+  const [newItem, setNewItem] = useState('');
+
+  const setAndSaveItems = (newItems) => {
+    setItems(newItems);
+    localStorage.setItem('shoppinglist', JSON.stringify(newItems));
+  };
+
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const myNewItem = { id, checked: false, item };
+    const listItems = [...items, myNewItem];
+    setAndSaveItems(listItems);
+  };
+
   const handleCheck = (id) => {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item,
     );
-    setItems(listItems);
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+    setAndSaveItems(listItems);
   };
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setItems(listItems);
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+    setAndSaveItems(listItems);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItem) return;
+    addItem(newItem);
+    setNewItem('');
   };
   return (
     <div className="App">
       <Headers title="To Do List" />
+      <AddItem newItem={newItem} setNewItem={setNewItem} handleSubmit={handleSubmit} />
       <Content items={items} handleDelete={handleDelete} handleCheck={handleCheck} />
       <Footer length={items.length} />
     </div>
